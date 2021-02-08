be082afb14458040d3a5565315a1feb048fcdb38
bosdos12 <66145209+bosdos12@users.noreply.github.com>
2021-02-08 22:18:10 +0100

    Create DataArchitecture.js

---
+++ DataArchitecture.js
@@ -0,0 +1,10 @@
+theusertoinsert = {
+    "email": email_gotten_su,
+    "username": username_gotten_su,
+    "birthyear": birthyear_gotten_su,
+    "password": password_gotten_su,
+    "friendsamount": 0,
+    "friends": "",
+    "userbio": "",
+    "createdon": datetime.datetime.now()
+}
